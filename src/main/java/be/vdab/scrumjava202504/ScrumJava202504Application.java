1915988eec96cf8bb517d19a91d852dd988d3282
KennethVG <kenneth.vangijsel@gmail.com>
2025-04-30 13:41:26 +0200

    Initial commit

---
+++ src/main/java/be/vdab/scrumjava202504/ScrumJava202504Application.java
@@ -0,0 +1,13 @@
+package be.vdab.scrumjava202504;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumJava202504Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrumJava202504Application.class, args);
+    }
+
+}
