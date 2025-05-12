7b08148b063ab7b4776218990056ae49ce01b0b9
Emile <emile.vandendorpe@vdabcampus.be>
2025-05-12 13:18:05 +0200

    Commit ProductDTO with all the information to pick an order

---
+++ src/main/java/be/vdab/scrumjava202504/ProductDTO.java
@@ -0,0 +1,48 @@
+package be.vdab.scrumjava202504;
+
+/**
+ * Represents an order with all the necessary information for pickup
+ */
+
+public class ProductDTO {
+    private String shelf;
+    private int position;
+    private String name;
+    private int quantity;
+
+    public ProductDTO(String shelf, int position, String naam, int quantity) {
+        this.shelf = shelf;
+        this.position = position;
+        this.name = naam;
+        this.quantity = quantity;
+    }
+
+    /**
+     * Returns the shelf where the product is stored in the warehouse
+     */
+    public String getShelf() {
+        return shelf;
+    }
+
+    /**
+     * Returns the position where the product is stored in the warehouse (ex. shelf A position 1)
+     */
+    public int getPosition() {
+        return position;
+    }
+
+    /**
+     * Returns the name of the product
+     */
+    public String getNaam() {
+        return name;
+    }
+
+    /**
+     * Returns the quantity of the ordered amount of the product
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+}
