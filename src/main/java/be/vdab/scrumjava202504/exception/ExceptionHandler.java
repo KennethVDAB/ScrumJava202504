441f2110de1832078e8dde8fc5a64c1e620f93f9
yalozok <olga.kozmovskaia@yandex.ru>
2025-05-12 11:41:28 +0200

    Add ExceptionHandler

---
+++ src/main/java/be/vdab/scrumjava202504/exception/ExceptionHandler.java
@@ -0,0 +1,17 @@
+package be.vdab.scrumjava202504.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(final Exception e) {
+        log.warn("Error: ", e);
+        return new ErrorResponse(e.getMessage());
+    }
+}
