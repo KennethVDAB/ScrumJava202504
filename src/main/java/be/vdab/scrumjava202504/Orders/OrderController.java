530911ae537936f6260d10adf6503ceb61515a73
matthewDerous <matthew.derous@vdabcampus.be>
2025-05-12 13:03:31 +0200

    aanmaak html introscreen

--- src/main/java/be/vdab/scrumjava202504/Orders/OrderController.java
+++ src/main/java/be/vdab/scrumjava202504/Orders/OrderController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/Bestelling")
+@RequestMapping("api/Order")
 public class OrderController {
     private final OrderService orderService;
 
