a4aa7a345c9f272485eb30e2f9b8dfa26973d611
yalozok <olga.kozmovskaia@yandex.ru>
2025-05-12 14:49:47 +0200

    Resolve conflict OrderController

--- src/main/java/be/vdab/scrumjava202504/Orders/OrderController.java
+++ src/main/java/be/vdab/scrumjava202504/Orders/OrderController.java
@@ -1,18 +1,21 @@
 package be.vdab.scrumjava202504.Orders;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/orders")
-@RequiredArgsConstructor
+@RequestMapping("api/order")
 public class OrderController {
     private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @GetMapping("/count")
     public long getOrdersCount() {
         return orderService.getOrdersCount();
     }
 }
+
