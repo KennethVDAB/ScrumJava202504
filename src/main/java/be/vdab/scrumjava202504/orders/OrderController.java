af62233c5a603a99b5005b122564353f31ebb2e7
yalozok <olga.kozmovskaia@yandex.ru>
2025-05-12 11:40:42 +0200

    Add OrderController

---
+++ src/main/java/be/vdab/scrumjava202504/orders/OrderController.java
@@ -0,0 +1,18 @@
+package be.vdab.scrumjava202504.orders;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @GetMapping("/count")
+    public long getOrdersCount() {
+        return orderService.getOrdersCount();
+    }
+}
