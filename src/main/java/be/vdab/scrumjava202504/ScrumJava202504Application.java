e213be4991632a72628d00158a2614aa4cdcd22d
Jonas <jonas.minnebo@vdabcampus.be>
2025-05-23 13:07:54 +0200

    Update stock quantity calculation in OrderService to use picked quantity

--- src/main/java/be/vdab/scrumjava202504/ScrumJava202504Application.java
+++ src/main/java/be/vdab/scrumjava202504/ScrumJava202504Application.java
@@ -2,12 +2,18 @@ package be.vdab.scrumjava202504;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ScrumJava202504Application {
+public class ScrumJava202504Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ScrumJava202504Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ScrumJava202504Application.class);
+    }
 }
